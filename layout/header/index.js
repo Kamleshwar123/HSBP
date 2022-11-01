27b2a400a04473c52bef63f664f1631c6179c645
Kamleshwar Pandey <k.kamleshwar@abym.in>
2022-11-01 18:02:18 +0530

    changes in header

--- layout/header/index.js
+++ layout/header/index.js
@@ -1,63 +1,37 @@
 import Sidebar from "./sidebar";
-import ThemeProvider from "@mui/styles";
 import Link from "next/link";
-import { makeStyles } from "@mui/styles";
-import { Container } from '@mui/material'
-const useStyles = makeStyles((ThemeProvider) => ({
-    header: {
-        background: 'red',
-        color: 'black',
-        height: '70px',
-        background: '#fff',
-        boxShadow: '2px -6px 20px 0 #b2a9a9',
-        position: 'fixed',
-        width: '100%',
-    },
-    headerSection: {
-        height: '90px',
-        position: 'relative',
-    },
-    headerContain: {
-        height: '70px',
-        verticalAlign: 'middle',
-        display: 'flex',
-        alignItems: 'center',
-    },
-    cartImg: {
-        // width: "65%",
-        height: "auto",
-      },
-}
-));
+  
 export default function Header() {
-    const classes = useStyles(ThemeProvider);
     return (
         <>
-            <section className={classes.headerSection}>
-                <div className={classes.header}>
-                    <Sidebar />
-                    <Container className={classes.headerContain}>
-                        <div className='home-logo'>
-                            <Link href={'/'}>
-                                <img
-                                    src='/././assets/images/logo.webp'
-                                    alt='HSBP Logo'
-                                    width={'181'}
-                                    height={'63'}
-                                />
-                            </Link>
-                        </div>
-                        <div className="right-header">
-                            <img
-                            src={'/././assets/images/cart.png'}
-                            alt='HSBP Cart'
-                            width={'34'}
-                            height={'40'}
-                            />
-                        </div>
-                    </Container>
-                </div>
-            </section>
+            <header>
+          <Sidebar />
+          <div className='container'>
+        <div className='header-nav'>
+          <div className='header-nav-left'>
+           <div className='logo'>
+           <Link href={'/'}>
+              <img src={'assets/images/logo.webp'} alt="logo" height={'100%'} width={'100%'} />
+            </Link>
+           </div>
+        </div>
+        <div className='header-nav-right'>
+      <div className='cart-icon'>
+          <img src={'assets/images/cart.webp'} alt="cart" height={'100%'} width={'100%'}  />
+      </div>
+      <div className='app-icon'>
+          <p>DOWNLOAD APP</p>
+          <Link href={'/'}>
+          <img src={'assets/images/app-s.webp'} alt="app" height={'100%'} width={'100%'}  />
+     </Link>
+     <Link href={'/'}>
+          <img src={'assets/images/g-app.webp'} alt="app" height={'100%'} width={'100%'}  />
+         </Link>
+      </div>
+</div>
+      </div>
+      </div>
+     </header>    
         </>
 
     );
--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,10 @@
 import "../styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import"../styles/style.css";
 import React from "react";
 import App from "next/app";
-import { ThemeProvider } from "@mui/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
 import Head from "next/head";
 
-const theme = createTheme({
-  typography: {
-    // Use the system font instead of the default Roboto font.
-    fontFamily: ["Inter", "sans-serif"].join(","),
-  },
-  palette: {
-    primary: {
-      main: "#296ba0",
-    },
-    black: {
-      main: "#000000",
-    },
-    companyBlue: {
-      backgroundColor: "#65CFE9",
-      color: "#fff",
-    },
-    companyRed: {
-      backgroundColor: "#E44D69",
-      color: "#000",
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: "#fff",
-    },
-  },
-});
 function MyApp({ Component, pageProps }) {
   MyApp.getInitialProps = async (appContext) => {
     // calls page's `getInitialProps` and fills `appProps.pageProps`
@@ -49,10 +19,8 @@ function MyApp({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+    
         <Component {...pageProps} />
-      </ThemeProvider>
     </>
   );
 }
